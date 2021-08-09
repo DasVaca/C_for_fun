47f2b1f865720160c95900b25d1ec6343a4fb455
DasVaca <sebastian23082000@gmail.com>
2021-08-09 13:58:29 -0400

    snake: better draw_snake
    It keeps track of the last position so it clear it before printing the new snake

--- snake_classic/graphics.c
+++ snake_classic/graphics.c
@@ -1,13 +1,27 @@
 #include "graphics.h"
 
+
 void draw_food (WINDOW * win, int y, int x) {
     mvwaddch(win, y, x, ACS_BLOCK | A_BLINK);
 }
 
 void draw_snake(WINDOW * win, struct snake_t * snake) {
+    static int last_y = -1;
+    static int last_x = -1;
+
+    if (last_y > 0 && last_x > 0) {
+        wattron(win, COLOR_PAIR(COLOR_BLACK));
+        mvwaddch(win, last_y, last_x, ' ');
+        wattroff(win, COLOR_PAIR(COLOR_BLACK));
+    }
+
     struct snake_body_t * traveler = snake->body;
 
-    do {
+    while (traveler->next) {
         mvwaddch(win, traveler->y, traveler->x, ACS_BLOCK);
-    } while ((traveler = traveler->next));
+        traveler = traveler->next;
+    }
+
+    last_y = traveler->y;
+    last_x = traveler->x;
 }
